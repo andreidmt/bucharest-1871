5084836e9a798f8dfc8865706e18b041bd572ef5
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2018-12-03 04:01:15 +0100

    Create empty POI edit page

---
+++ src/poi-item.page/poi-item.page.jsx
@@ -0,0 +1,9 @@
+const debug = require("debug")("Bucharest1871:POIItemPage")
+
+import React from "react"
+
+class POIItemPage extends React.Component {
+  render = () => <div>asdasd</div>
+}
+
+export { POIItemPage }
