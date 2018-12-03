bd3589c35174048c4f54dd5bdb953445be192f0e
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2018-12-03 04:01:51 +0100

    Remove explicit created history. Initialize redux state and router

--- src/index.jsx
+++ src/index.jsx
@@ -2,28 +2,43 @@ const debug = require("debug")("Bucharest1871:Index")
 
 import React from "react"
 import { render } from "react-dom"
-import { createStore, combineReducers, applyMiddleware } from "redux"
+import { createStore, combineReducers } from "redux"
 import { Provider } from "react-redux"
-import { BrowserRouter, Route } from "react-router-dom"
-import { routerReducer, routerMiddleware } from "react-router-redux"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { routerReducer } from "react-router-redux"
+
+import { getPath } from "./core/router.helper"
+import { DefaultLayout } from "./default.layout/default.layout"
 
 import { HomePage } from "./home.page/home.page"
-import { POIList } from "./home.page/poi-list.section/poi-list.state"
+
+import { POIListPage } from "./poi-list.page/poi-list.page"
+
+import { POIList } from "./poi-list.page/poi-list.section/poi-list.state"
+import { DefautLayoutPOIList } from "./default.layout/default.state"
+
+import { POIItemPage } from "./poi-item.page/poi-item.page"
 
 import "./index.css"
 
 const store = createStore(
   combineReducers({
+    [DefautLayoutPOIList.name]: DefautLayoutPOIList.reducer,
     [POIList.name]: POIList.reducer,
     router: routerReducer,
-  }),
-  applyMiddleware(routerMiddleware(history))
+  })
 )
 
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <Route exact={true} path="/" component={HomePage} />
+      <DefaultLayout>
+        <Switch>
+          <Route exact={true} path="/" component={HomePage} />
+          <Route exact={true} path={getPath("poi")} component={POIListPage} />
+          <Route path={getPath("poi:item")} component={POIItemPage} />
+        </Switch>
+      </DefaultLayout>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
