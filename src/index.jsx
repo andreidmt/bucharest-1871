9545fe1ac8414a7413b0dece67f88fbd5fb48d6e
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2018-12-10 09:19:57 +0100

    Remove container state lists

--- src/index.jsx
+++ src/index.jsx
@@ -8,23 +8,19 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { routerReducer } from "react-router-redux"
 
 import { getPath } from "./core/router.helper"
-import { DefaultLayout } from "./default.layout/default.layout"
+import { Layout } from "./layout/layout"
+
+import { LayoutPOIList } from "./layout/layout.state"
 
 import { HomePage } from "./home.page/home.page"
-
 import { POIListPage } from "./poi-list.page/poi-list.page"
-
-import { POIList } from "./poi-list.page/poi-list.section/poi-list.state"
-import { DefautLayoutPOIList } from "./default.layout/default.state"
-
 import { POIItemPage } from "./poi-item.page/poi-item.page"
 
 import "./index.css"
 
 const store = createStore(
   combineReducers({
-    [DefautLayoutPOIList.name]: DefautLayoutPOIList.reducer,
-    [POIList.name]: POIList.reducer,
+    [LayoutPOIList.name]: LayoutPOIList.reducer,
     router: routerReducer,
   })
 )
@@ -32,13 +28,13 @@ const store = createStore(
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <DefaultLayout>
+      <Layout>
         <Switch>
           <Route exact={true} path="/" component={HomePage} />
-          <Route exact={true} path={getPath("poi")} component={POIListPage} />
-          <Route path={getPath("poi:item")} component={POIItemPage} />
+          <Route exact={true} path={getPath("pois")} component={POIListPage} />
+          <Route path={getPath("pois:item")} component={POIItemPage} />
         </Switch>
-      </DefaultLayout>
+      </Layout>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
