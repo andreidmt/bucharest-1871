9a2a350ba2eab0dcc0f967ec8a2d1c35c1a368b9
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2018-12-19 09:16:40 +0100

    Change text

--- src/home.page/home.page.jsx
+++ src/home.page/home.page.jsx
@@ -5,7 +5,7 @@ const debug = require("debug")("Bucharest1871:HomePage")
 import * as React from "react"
 
 class HomePage extends React.Component<{}> {
-  render = (): React.Node => <div />
+  render = (): React.Node => <div>Hello TomTom!</div>
 }
 
 export { HomePage }
